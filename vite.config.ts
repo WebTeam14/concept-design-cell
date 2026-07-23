a49b0b2843978a17a20299cfed80d6fdd78a1140
WebTeam14 <web.technoriya@gmail.com>
2026-07-23 11:09:26 +0500

    base path changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/concept-design-cell/',
+  base: '/',
   assetsInclude: ['**/*.JPG', '**/*.JPEG', '**/*.PNG'],
   server: {
     host: "::",
